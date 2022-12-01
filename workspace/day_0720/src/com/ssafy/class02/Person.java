495cbc8098a6ad9b0a6230c89a7113782264f81f
JayChoi <chn9801@gmail.com>
2022-12-01 19:54:25 +0900

    1201

---
+++ workspace/day_0720/src/com/ssafy/class02/Person.java
@@ -0,0 +1,11 @@
+package com.ssafy.class02;
+
+
+// 이러한 다양한 자료형 가질 수 있는 자료형을 만들고 싶다
+public class Person {
+	public static void main(String[] args) {
+		String name;
+		int age;
+		String hobby;
+	}
+}
